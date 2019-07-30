e74ca7bc4093bc86e5305c524c8c5dc62657afe4
seriiin <csl602@likelion.org>
2019-07-31 00:57:14 +0900

    add ignore

--- second/secondproject/settings.py
+++ second/secondproject/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0eka5wwjzo2evks_+x318pv*93q9!#40t1rti0(j8q=e_#5(-k'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
